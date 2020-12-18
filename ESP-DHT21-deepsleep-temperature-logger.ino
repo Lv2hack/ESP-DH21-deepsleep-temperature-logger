23fa977a9004101fbcc6e961125271b4d8d59363
Alex T <alext@pobox.com>
2020-12-18 15:46:03 -0700

    Update ESP-DHT21-deepsleep-temperature-logger.ino

--- ESP-DHT21-deepsleep-temperature-logger.ino
+++ ESP-DHT21-deepsleep-temperature-logger.ino
@@ -44,8 +44,7 @@ char strdeepSleepSecs[8] = "1200";  //used for reading from json
 int analogValue;  // Analog pin reading
 
 // The ID below comes from Google Sheets. See how to get it here: http://embedded-lab.com/blog/post-data-google-sheets-using-esp8266
-char GScriptID[60] = "scrip_id_here";  // Default setting for GScriptID
-//char GScriptID[60] = "<YOUR-GSCRIPT-ID-HERE>";  // Default setting for GScriptID
+char GScriptID[60] = "<YOUR-GSCRIPT-ID-HERE>";  // Default setting for GScriptID
 
 const char* host = "script.google.com";
 const int httpsPort = 443;
